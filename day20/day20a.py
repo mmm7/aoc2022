f6ff80e258833d8e97d426aaa2d79ae3da2e3673
Mate <mate@noreply.com>
2022-12-20 07:05:40 +0100

    day20

---
+++ day20/day20a.py
@@ -0,0 +1,25 @@
+from input import I
+
+I = [(i,x) for i,x in enumerate(I)]
+print(I)
+L=len(I)
+print(L)
+
+for num in range(L):
+    for i in range(L):
+        if I[i][0]==num: break
+    x = I[i][1]
+    elem = I.pop(i)
+    newpos = (i+x) % (L-1)
+    I.insert(newpos,elem)
+    #print(x,i,newpos,I)
+
+print(x,i,newpos,I)
+
+for zero in range(L):
+    if I[zero][1] == 0: break
+px,py,pz = (zero+1000)%L, (zero+2000)%L, (zero+3000)%L
+x,y,z = I[px][1], I[py][1], I[pz][1]
+print(px,py,pz)
+print(x,y,z)
+print(x+y+z)
